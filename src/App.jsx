cc4e741ab5df8f419385e23358db4b06ca01b5ba
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2024-10-14 17:37:24 -0500

    Solucionando importación

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Fonts from './components/Fonts'
 import { FilterContextProvider } from './context/FilterContext'
 import AppRouter from './routes/AppRouter'
 
 function App() {
   return (
     <FilterContextProvider>
-      <Fonts />
       <AppRouter />
     </FilterContextProvider>
   )
